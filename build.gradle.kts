d6084e1552ad4af557b969cef912bf212b38dcb5
Neeme Praks <neeme@praks.net>
2025-04-03 22:26:19 +0300

    Prepare to release 1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 allprojects {
   group = "dev.nemecec.kotlinlogging.compiletimeplugin"
-  version = "1.1.1"
+  version = "1.1.2"
 }
 
 tasks.named("dokkaHtmlMultiModule", DokkaMultiModuleTask::class.java).configure {
