0fec8b964880a96517ca064bb11f16ac68fcd445
Neeme Praks <neeme@praks.net>
2025-09-17 21:10:56 +0300

    Update to Kotlin 2.2.20 and release 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 allprojects {
   group = "dev.nemecec.kotlinlogging.compiletimeplugin"
-  version = "1.3.1"
+  version = "1.4.0"
 }
 
 tasks.named("dokkaHtmlMultiModule", DokkaMultiModuleTask::class.java).configure {
